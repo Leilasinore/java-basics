acd1f9a855e31d444132271c17aecd9032ab1e79
Leila Sinore <leilasinore99@gmail.com>
2023-04-22 22:40:01 +0300

    basics

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+import java.awt.*;
+
+public class Main {
+    public static void main(String[] args){
+        int num1= 2;
+        float labValue= 12.5F;
+        long numViews=1_123_124_123;
+        int x=5;
+        int y=3;
+        Point location= new Point(x=2,y=9);
+        String name="Leila";
+        int lengthOfName=name.length();
+        Point location2= location;
+        location.x=5;
+        int result= 2+4;
+        System.out.println(result);
+        System.out.println(location2);
+      System.out.println(numViews);
+        System.out.println(name);
+        System.out.println(lengthOfName);
+    }
+}
