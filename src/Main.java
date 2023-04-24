6803ac008654c10c57f282516b74a9426886ae01
Leila Sinore <leilasinore99@gmail.com>
2023-04-24 23:08:24 +0300

    classes,methods,oop

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,15 @@ import java.awt.*;
 import java.text.NumberFormat;
 import java.util.Scanner;
 
+class Calculator {
+    public int add(int number1, int number2){
+        System.out.println("We are adding numbers");
+        int r =number1 + number2;
+        System.out.println(r);
+        return r;
+    }
+}
+
 public class Main {
     public static void main(String[] args){
         //different types of variables in Java: primitive and Reference Types
@@ -58,28 +67,30 @@ public class Main {
 
         //task
         //building a mortgage calculator
-        Scanner principalInput= new Scanner(System.in);
-        System.out.print("Principal : ");
-        double principal=principalInput.nextDouble();
-        //get annual interest rate from user
-        Scanner interestRateInput=new Scanner(System.in);
-        System.out.print("Annual Interest Rate : ");
-        double interest=interestRateInput.nextDouble();
-        //get period in years
-        Scanner period=new Scanner(System.in);
-        System.out.println("Period : ");
-        double periodInYears=period.nextDouble();
-
-        //convert interest rate to monthly
-        double monthlyRate= interest/1200;
-        double numberOfPayments= periodInYears *12 ;
-//        double result1cal= (1+monthlyRate);
-        double mortgage= ((Math.pow((1+monthlyRate),numberOfPayments) *monthlyRate)/((Math.pow((1+monthlyRate),numberOfPayments))-1))*principal;
+//        Scanner principalInput= new Scanner(System.in);
+//        System.out.print("Principal : ");
+//        double principal=principalInput.nextDouble();
 
 
-        //format output
-        String mortgageFormatted = NumberFormat.getCurrencyInstance().format(mortgage);
-        System.out.println(mortgageFormatted);
+//        //get annual interest rate from user
+//        Scanner interestRateInput=new Scanner(System.in);
+//        System.out.print("Annual Interest Rate : ");
+//        double interest=interestRateInput.nextDouble();
+//        //get period in years
+//        Scanner period=new Scanner(System.in);
+//        System.out.println("Period : ");
+//        double periodInYears=period.nextDouble();
+
+//        //convert interest rate to monthly
+//        double monthlyRate= interest/1200;
+//        double numberOfPayments= periodInYears *12 ;
+////        double result1cal= (1+monthlyRate);
+//        double mortgage= ((Math.pow((1+monthlyRate),numberOfPayments) *monthlyRate)/((Math.pow((1+monthlyRate),numberOfPayments))-1))*principal;
+
+
+//        //format output
+//        String mortgageFormatted = NumberFormat.getCurrencyInstance().format(mortgage);
+//        System.out.println(mortgageFormatted);
 
         //conditional statements:if statements
         int temperature=32;
@@ -108,28 +119,72 @@ public class Main {
             default:
                 System.out.println("you are a guest");
         }
+
+
         //FizzBuzz Exercise
-        Scanner randomInput = new Scanner(System.in);
-        int userInput=randomInput.nextByte();
-        int modResult= userInput % 5;
-        int modResult2= userInput % 3;
+//        Scanner randomInput = new Scanner(System.in);
+//        int userInput=randomInput.nextByte();
+//        int modResult= userInput % 5;
+//        int modResult2= userInput % 3;
+//
+//        if (modResult2==0 && modResult==0) {
+//            System.out.println("FizzBuzz");
+//        }
+//        else if(modResult==0){
+//            System.out.println("Fizz");
+//        }
+//        else if (modResult2==0) {
+//            System.out.println("Buzz");
+//        }
+//        else System.out.println(userInput);
+//
+//        //loops;for loop
+//        for(int i=0;i<5;i++){
+//            System.out.println("You are on number" + i);
+//        }
+//        //while loop
+//       int j=0;
+//        while(j<5){
+//            System.out.println("Number" + j);
+//            j++;
+//        }
+//        //for each loop...used to iterate over an array of items
+//        String [] names= {"Leila","Sinore","Wuantai"};
+//        for ( String k : names)
+//            System.out.println(k);
 
-        if (modResult2==0 && modResult==0) {
-            System.out.println("FizzBuzz");
-        }
-        else if(modResult==0){
-            System.out.println("Fizz");
-        }
-        else if (modResult2==0) {
-            System.out.println("Buzz");
-        }
-        else System.out.println(userInput);
-
-        //loops;for loop
-        for(int i=0;i<5;i++){
-            System.out.println("You are on number" + i);
-        }
-        //while loop
+        //improved mortgage calculator with user input validation features
+//        Scanner scanner5= new Scanner(System.in);
+//        System.out.print("Principal : ");
+//        double principal=scanner5.nextDouble();
+//        while(principal>1000 && principal< 1000000){
+//            System.out.print("Interest Rate : ");
+//            float interestRate=scanner5.nextFloat();
+//            while(interestRate>0 && interestRate<30){
+//                System.out.print("Period: ");
+//                int loanPeriod=scanner5.nextInt();
+//                while(loanPeriod>0&& loanPeriod<=30){
+//                    //convert interest rate to monthly
+//                    double monthlyRate= interestRate/1200;
+//                    double numberOfPayments= loanPeriod *12 ;
+//                    double mortgage= ((Math.pow((1+monthlyRate),numberOfPayments) *monthlyRate)/((Math.pow((1+monthlyRate),numberOfPayments))-1))*principal;
+//                    //format output
+//                    String mortgageFormatted = NumberFormat.getCurrencyInstance().format(mortgage);
+//                    System.out.println(mortgageFormatted);
+//                    if(mortgage!=0){
+//                        System.out.println("That is your mortgage");
+//                        break;
+//                    }
+//
+//                }
+//                System.out.println("Enter number between 1 and 30");
+//            }
+//            System.out.println("Enter a number between 0 and 30");
+//        }
+//
+//        System.out.println("Enter a number between 1,000 and 1000000");
+        Calculator calc= new Calculator();
+        int resultanwer= calc.add(2,3);
 
     }
 }
